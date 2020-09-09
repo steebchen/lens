2ecb177ebbac9c5cf7feb81d4ba80ee86546496b
Nilu <nilubava@gmail.com>
2020-09-09 14:53:10 +0200

    feat: add footer and header to theme

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import styled, { css } from 'styled-components'
+
+
+const Header = () => (
+    <header>
+        <a>Logo</a>
+    </header>
+)
+
+export default Header
