054774c546601aac0c22ed0880d02cb467d84379
Matej Hrescak <matej@hrescak.com>
2021-01-05 13:23:46 -0500

    botched export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default } from './theme'
-export * from './components/BaseStyle'
+export { default as BaseStyle } from './components/BaseStyle'
 export { default as Header } from './components/header'
 export { default as Footer } from './components/footer'
