1fc2b40747434e32a07c6a9291c3598bd32c084d
Matej Hrescak <matej@hrescak.com>
2021-01-05 13:56:33 -0500

    absolute paths to fonts

--- src/components/BaseStyle/font-face.ts
+++ src/components/BaseStyle/font-face.ts
@@ -6,8 +6,8 @@ const fontFace = css`
   @font-face {
     font-family: "Jetbrains Mono";
     font-display: swap;
-    src: url("/fonts/jetbrains-mono/regular.woff2") format("woff2"),
-      url("/fonts/jetbrains-mono/regular.woff") format("woff");
+    src: url("https://www.prisma.io/fonts/jetbrains-mono/regular.woff2") format("woff2"),
+      url("https://www.prisma.io/fonts/jetbrains-mono/regular.woff") format("woff");
   }
 
   /*---- BARLOW BOLD ----*/
@@ -16,8 +16,8 @@ const fontFace = css`
     font-family: "Barlow";
     font-style: normal;
     font-weight: 700;
-    src: url("/fonts/barlow-v5-latin/barlow-v5-latin-700.woff2") format("woff2"),
-      url("/fonts/barlow-v5-latin/barlow-v5-latin-700.woff") format("woff");
+    src: url("https://www.prisma.io/fonts/barlow-v5-latin/barlow-v5-latin-700.woff2") format("woff2"),
+      url("https://www.prisma.io/fonts/barlow-v5-latin/barlow-v5-latin-700.woff") format("woff");
   }
 
   /*---- INTER ----*/
@@ -27,16 +27,16 @@ const fontFace = css`
     font-style: normal;
     font-weight: 400;
     font-display: swap;
-    src: url("/fonts/inter/Inter-Regular.woff2?v=3.15") format("woff2"),
-      url("/fonts/inter/Inter-Regular.woff?v=3.15") format("woff");
+    src: url("https://www.prisma.io/fonts/inter/Inter-Regular.woff2?v=3.15") format("woff2"),
+      url("https://www.prisma.io/fonts/inter/Inter-Regular.woff?v=3.15") format("woff");
   }
   @font-face {
     font-family: "Inter";
     font-style: italic;
     font-weight: 400;
     font-display: swap;
-    src: url("/fonts/inter/Inter-Italic.woff2?v=3.15") format("woff2"),
-      url("/fonts/inter/Inter-Italic.woff?v=3.15") format("woff");
+    src: url("https://www.prisma.io/fonts/inter/Inter-Italic.woff2?v=3.15") format("woff2"),
+      url("https://www.prisma.io/fonts/inter/Inter-Italic.woff?v=3.15") format("woff");
   }
 
   @font-face {
@@ -44,16 +44,16 @@ const fontFace = css`
     font-style: normal;
     font-weight: 500;
     font-display: swap;
-    src: url("/fonts/inter/Inter-Medium.woff2?v=3.15") format("woff2"),
-      url("/fonts/inter/Inter-Medium.woff?v=3.15") format("woff");
+    src: url("https://www.prisma.io/fonts/inter/Inter-Medium.woff2?v=3.15") format("woff2"),
+      url("https://www.prisma.io/fonts/inter/Inter-Medium.woff?v=3.15") format("woff");
   }
   @font-face {
     font-family: "Inter";
     font-style: italic;
     font-weight: 500;
     font-display: swap;
-    src: url("/fonts/inter/Inter-MediumItalic.woff2?v=3.15") format("woff2"),
-      url("/fonts/inter/Inter-MediumItalic.woff?v=3.15") format("woff");
+    src: url("https://www.prisma.io/fonts/inter/Inter-MediumItalic.woff2?v=3.15") format("woff2"),
+      url("https://www.prisma.io/fonts/inter/Inter-MediumItalic.woff?v=3.15") format("woff");
   }
 
   @font-face {
@@ -61,16 +61,16 @@ const fontFace = css`
     font-style: normal;
     font-weight: 600;
     font-display: swap;
-    src: url("/fonts/inter/Inter-SemiBold.woff2?v=3.15") format("woff2"),
-      url("/fonts/inter/Inter-SemiBold.woff?v=3.15") format("woff");
+    src: url("https://www.prisma.io/fonts/inter/Inter-SemiBold.woff2?v=3.15") format("woff2"),
+      url("https://www.prisma.io/fonts/inter/Inter-SemiBold.woff?v=3.15") format("woff");
   }
   @font-face {
     font-family: "Inter";
     font-style: italic;
     font-weight: 600;
     font-display: swap;
-    src: url("/fonts/inter/Inter-SemiBoldItalic.woff2?v=3.15") format("woff2"),
-      url("/fonts/inter/Inter-SemiBoldItalic.woff?v=3.15") format("woff");
+    src: url("https://www.prisma.io/fonts/inter/Inter-SemiBoldItalic.woff2?v=3.15") format("woff2"),
+      url("https://www.prisma.io/fonts/inter/Inter-SemiBoldItalic.woff?v=3.15") format("woff");
   }
 
   @font-face {
@@ -78,16 +78,16 @@ const fontFace = css`
     font-style: normal;
     font-weight: 700;
     font-display: swap;
-    src: url("/fonts/inter/Inter-Bold.woff2?v=3.15") format("woff2"),
-      url("/fonts/inter/Inter-Bold.woff?v=3.15") format("woff");
+    src: url("https://www.prisma.io/fonts/inter/Inter-Bold.woff2?v=3.15") format("woff2"),
+      url("https://www.prisma.io/fonts/inter/Inter-Bold.woff?v=3.15") format("woff");
   }
   @font-face {
     font-family: "Inter";
     font-style: italic;
     font-weight: 700;
     font-display: swap;
-    src: url("/fonts/inter/Inter-BoldItalic.woff2?v=3.15") format("woff2"),
-      url("/fonts/inter/Inter-BoldItalic.woff?v=3.15") format("woff");
+    src: url("https://www.prisma.io/fonts/inter/Inter-BoldItalic.woff2?v=3.15") format("woff2"),
+      url("https://www.prisma.io/fonts/inter/Inter-BoldItalic.woff?v=3.15") format("woff");
   }
 `;
 
